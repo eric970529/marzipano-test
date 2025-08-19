5d0caf83a7427d47806161829746fd35d1123d28
eric970529 <pig860529@gmail.com>
2025-08-19 10:01:38 +0800

    Update data.js

--- Project/忠明路0816/data.js
+++ Project/忠明路0816/data.js
@@ -616,6 +616,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
